b20a8c68358a8f0747edffc69339c808cd810b45
JIAYU LIN <louis70109@gmail.com>
2019-12-14 22:54:09 +0800

    add testing and annotation node_modules

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testEnvironment: 'node',
   moduleFileExtensions: ['js'],
-  transformIgnorePatterns: ['/node_modules/'],
+  // transformIgnorePatterns: ['/node_modules/'],
   testMatch: [
     '<rootDir>/**/__tests__/**/*.js',
     '<rootDir>/**/*.{spec,test}.js',
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,14 @@
-const App = require('.');
+const sut = require('./operation');
 
-describe('index.js', () => {
-  it('should be defined', () => {
-    expect(App).toBeDefined();
-  });
+test.each([
+  ['富邦', '台北富邦勇士'],
+  ['夢想家', '寶島夢想家'],
+])('selectTeam; %s; %s', async (team, expected) => {
+  // arrange
+
+  // action
+  const actual = sut.selectTeam(team);
+
+  // assert
+  expect(actual.name).toEqual(expected);
 });
